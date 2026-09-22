760b2af830cce027d371c0495a705f21b93dfad2
faatiss <fatimagarciamendozaa@gmail.com>
2026-09-22 08:49:01 -0600

    Primer Avance
    Primer Avance

---
+++ pes/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(vertical, 0f, -horizontal);
+
+        controller.Move(movement.normalized * speed * Time.deltaTime);
+
+
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
